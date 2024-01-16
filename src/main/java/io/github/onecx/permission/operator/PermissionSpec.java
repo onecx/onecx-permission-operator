38db49f8415f4f1b3add336c317db575a81d91a8
Andrej Petras <andrej@ajka-andrej.com>
2024-01-16 15:53:13 +0100

    feat: update spec (#2)

--- src/main/java/io/github/onecx/permission/operator/PermissionSpec.java
+++ src/main/java/io/github/onecx/permission/operator/PermissionSpec.java
@@ -1,6 +1,6 @@
 package io.github.onecx.permission.operator;
 
-import java.util.List;
+import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -12,7 +12,7 @@ public class PermissionSpec {
     private String appId;
 
     @JsonProperty("permissions")
-    private List<PermissionItemSpec> permissions;
+    private Map<String, Map<String, String>> permissions;
 
     public String getAppId() {
         return appId;
@@ -22,54 +22,12 @@ public class PermissionSpec {
         this.appId = appId;
     }
 
-    public List<PermissionItemSpec> getPermissions() {
+    public Map<String, Map<String, String>> getPermissions() {
         return permissions;
     }
 
-    public void setPermissions(List<PermissionItemSpec> permissions) {
+    public void setPermissions(Map<String, Map<String, String>> permissions) {
         this.permissions = permissions;
     }
 
-    public static class PermissionItemSpec {
-        @JsonProperty(value = "name")
-        private String name;
-        @JsonProperty(value = "resource", required = true)
-        private String resource;
-        @JsonProperty(value = "action", required = true)
-        private String action;
-        @JsonProperty(value = "description")
-        private String description;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getResource() {
-            return resource;
-        }
-
-        public void setResource(String resource) {
-            this.resource = resource;
-        }
-
-        public String getAction() {
-            return action;
-        }
-
-        public void setAction(String action) {
-            this.action = action;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-    }
 }
